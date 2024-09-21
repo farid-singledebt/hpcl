fd8bc8d4147be4841db88389de7d9d0817b14465
Farid Ansari <farid.ansari@singledebt.in>
2024-09-21 16:18:34 +0530

    update

--- data/presentation.js
+++ data/presentation.js
@@ -5,7 +5,7 @@ const presentationData = [
     img: "https://farid-singledebt.github.io/hpcl/images/presentation/channel_partners.jpg",
     body: "Join us as we embark on an adventure to discover the hidden beauty of nature.",
     overlay: "Step into adventure",
-    pdf: "../pdf/Channel_partners_as_Force_Multipliers.pdf",
+    pdf: "https://farid-singledebt.github.io/hpcl/pdf/Channel_partners_as_Force_Multipliers.pdf",
   },
   {
     title: "Mountain Serenity",
@@ -13,7 +13,7 @@ const presentationData = [
     img: "https://farid-singledebt.github.io/hpcl/images/presentation/CX.jpg",
     body: "The stillness of the mountain top provides a perfect retreat from the chaos of daily life.",
     overlay: "Breathe the silence",
-    pdf: "../pdf/Customer_Focused_Transformation.pdf",
+    pdf: "https://farid-singledebt.github.io/hpcl/pdf/Customer_Focused_Transformation.pdf",
   },
   {
     title: "A Winter Wonderland",
@@ -21,7 +21,7 @@ const presentationData = [
     img: "https://farid-singledebt.github.io/hpcl/images/presentation/journey.jpg",
     body: "In this magical landscape, every corner offers a breathtaking view of snow-laden scenery.",
     overlay: "Pure winter magic",
-    pdf: "../pdf/HPCL's_Ascent_to_an_Iconic_Brand.pdf",
+    pdf: "https://farid-singledebt.github.io/hpcl/pdf/HPCL's_Ascent_to_an_Iconic_Brand.pdf",
   },
   {
     title: "Chasing the Horizon",
@@ -29,7 +29,7 @@ const presentationData = [
     img: "https://farid-singledebt.github.io/hpcl/images/presentation/mission.jpg",
     body: "There's always a new horizon to explore, and the thrill of the unknown keeps us going.",
     overlay: "Endless possibilities await",
-    pdf: "../pdf/Mission_60_@_T25.pdf",
+    pdf: "https://farid-singledebt.github.io/hpcl/pdf/Mission_60_@_T25.pdf",
   },
 ];
 

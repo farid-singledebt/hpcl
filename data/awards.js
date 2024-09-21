9380cc8548c4f72d461884fa1dfc274194aea196
Farid Ansari <farid.ansari@singledebt.in>
2024-09-21 15:18:19 +0530

    update

--- data/awards.js
+++ data/awards.js
@@ -3,85 +3,307 @@ const awardsData = [
     SBU: "North Region",
     category: "Best Sales Performance",
     winner: "Alice Johnson",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/1.JPG",
   },
   {
     SBU: "West Region",
     category: "Customer Excellence",
     winner: "Michael Brown",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/2.JPG",
   },
   {
     SBU: "East Region",
     category: "Innovation Leader",
     winner: "Jessica Smith",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/3.JPG",
   },
   {
     SBU: "South Region",
     category: "Operational Excellence",
     winner: "David Lee",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/4.JPG",
   },
   {
     SBU: "Global Headquarters",
     category: "Leadership Award",
     winner: "Emily Davis",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/5.JPG",
   },
   {
     SBU: "APAC Region",
     category: "Rising Star",
     winner: "Sophia Wilson",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/6.JPG",
   },
   {
     SBU: "EMEA Region",
     category: "Team Collaboration",
     winner: "Liam Martin",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/7.JPG",
   },
   {
     SBU: "North Region",
     category: "Best Customer Retention",
     winner: "Ava Thompson",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/8.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5477.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5506.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5515.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5550.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5553.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5557.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5558.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5563.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5566.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5836.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5914.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5923.JPG",
+  },
+  {
+    SBU: "North Region",
+    category: "Best Customer Retention",
+    winner: "Ava Thompson",
+    img: "../images/awards/8D3A5928.JPG",
   },
   {
     SBU: "West Region",
     category: "Marketing Excellence",
     winner: "Noah White",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/9.JPG",
   },
   {
     SBU: "East Region",
     category: "Innovation in Technology",
     winner: "Isabella Garcia",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/10.JPG",
   },
   {
     SBU: "South Region",
     category: "Employee of the Year",
     winner: "James Martinez",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/11.JPG",
   },
   {
     SBU: "Global Headquarters",
     category: "Best Product Development",
     winner: "Mia Clark",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/12.JPG",
   },
   {
     SBU: "APAC Region",
     category: "Best Financial Performance",
     winner: "Alexander Lewis",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/13.JPG",
   },
   {
     SBU: "EMEA Region",
     category: "Sustainability Leader",
     winner: "Evelyn Walker",
-    img: "https://images.pexels.com/photos/17805250/pexels-photo-17805250/free-photo-of-black-and-white-photograph-of-a-snowy-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../images/awards/14.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/15.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/16.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/17.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/18.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/19.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/20.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/21.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/22.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/23.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/24.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/25.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/26.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/27.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/28.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/29.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/30.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/31.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/32.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/33.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/34.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/35.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/36.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/37.JPG",
+  },
+  {
+    SBU: "EMEA Region",
+    category: "Sustainability Leader",
+    winner: "Evelyn Walker",
+    img: "../images/awards/38.JPG",
   },
 ];
 
@@ -91,13 +313,24 @@ awardsData
   .map((item, index) => {
     awardsResult += `
     <tr>
-            <td>${index + 1}</td>
-            <td>${item.SBU}</td>
-            <td>${item.category}.</td>
-            <td>${item.winner}</td>
-            <td>View image</td>
-          </tr>
+      <td>${index + 1}</td>
+      <td>${item.SBU}</td>
+      <td>${item.category}.</td>
+      <td>${item.winner}</td>
+      <td style="cursor: pointer" class="view-image" data-img="${
+        item.img
+      }" data-bs-toggle="modal" data-bs-target="#imgModal">
+        View image <i class="fa-solid fa-eye"></i>
+      </td>
+    </tr>
   `;
   })
   .join("");
 awardsTable.innerHTML = awardsResult;
+
+document.querySelectorAll(".view-image").forEach((item) => {
+  item.addEventListener("click", function () {
+    const imgUrl = this.getAttribute("data-img");
+    document.querySelector("#imgModal img").src = imgUrl;
+  });
+});

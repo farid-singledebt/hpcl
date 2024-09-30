55db5dba0d2e9caa66b4fa3343baa172aa230436
Farid Ansari <farid.ansari@singledebt.in>
2024-09-30 16:53:50 +0530

    senior webp images

--- data/senior.js
+++ data/senior.js
@@ -1,57 +1,57 @@
 const seniorData = [
   {
-    img: "https://farid-singledebt.github.io/hpcl/images/seniors/1.JPG",
+    img: "https://farid-singledebt.github.io/hpcl/images/seniors/1.webp",
     name: "PRANAY KUMAR",
     designation: "Chief Executive Officer - HBL",
   },
   {
-    img: "https://farid-singledebt.github.io/hpcl/images/seniors/2.JPG",
+    img: "https://farid-singledebt.github.io/hpcl/images/seniors/2.webp",
     name: "SANJAY DASGUPTA",
     designation: "Chief General Manager - ERP",
   },
   {
-    img: "https://farid-singledebt.github.io/hpcl/images/seniors/3.JPG",
+    img: "https://farid-singledebt.github.io/hpcl/images/seniors/3.webp",
     name: "PRAVEEN KUMAR",
     designation: "Principal - HPMDI",
   },
   {
-    img: "https://farid-singledebt.github.io/hpcl/images/seniors/4.JPG",
+    img: "https://farid-singledebt.github.io/hpcl/images/seniors/4.webp",
     name: "BALAGURUNATHAN K",
     designation: "Chief General Manager - Tax",
   },
   {
-    img: "https://farid-singledebt.github.io/hpcl/images/seniors/5.JPG",
+    img: "https://farid-singledebt.github.io/hpcl/images/seniors/5.webp",
     name: "KOTA SREENIVASA RAO",
     designation: "Executive Director - Natural Gas",
   },
   {
-    img: "https://farid-singledebt.github.io/hpcl/images/seniors/6.JPG",
+    img: "https://farid-singledebt.github.io/hpcl/images/seniors/6.webp",
     name: "VIJAY SHRIKRISHNA AGASHE",
     designation: "Executive Director - Integrated Margin Management",
   },
   {
-    img: "https://farid-singledebt.github.io/hpcl/images/seniors/7.JPG",
+    img: "https://farid-singledebt.github.io/hpcl/images/seniors/7.webp",
     name: "AMITAVA MUKHOPADHYAY",
     designation:
       "Executive Director - Engineering Projects & Facilities Planning",
   },
   {
-    img: "https://farid-singledebt.github.io/hpcl/images/seniors/8.JPG",
+    img: "https://farid-singledebt.github.io/hpcl/images/seniors/8.webp",
     name: "RAJEEV GOEL",
     designation: "Executive Director - Corporate Social Responsibility & PRCC",
   },
   {
-    img: "https://farid-singledebt.github.io/hpcl/images/seniors/9.JPG",
+    img: "https://farid-singledebt.github.io/hpcl/images/seniors/9.webp",
     name: "SHUVENDU GUPTA",
     designation: "Chief Executive Officer",
   },
   {
-    img: "https://farid-singledebt.github.io/hpcl/images/seniors/10.JPG",
+    img: "https://farid-singledebt.github.io/hpcl/images/seniors/10.webp",
     name: "SANJAY MALHOTRA",
     designation: "Chief Executive Officer - HINCOL",
   },
   {
-    img: "https://farid-singledebt.github.io/hpcl/images/seniors/11.JPG",
+    img: "https://farid-singledebt.github.io/hpcl/images/seniors/11.webp",
     name: "KANURU SRINIVAS",
     designation:
       "Executive Director - Marketing Strategy and  Business Development",

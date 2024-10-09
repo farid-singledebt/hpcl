2d9ee46b653f56dd712087911b91bd14e2fcb0c9
Farid Ansari <farid.ansari@singledebt.in>
2024-10-09 10:13:42 +0530

    leadership images path

--- data/leadership.js
+++ data/leadership.js
@@ -1,36 +1,36 @@
 const leadershipData = [
   {
-    img: "../images/leadership/CMD.webp",
+    img: "https://82.112.226.178/images/leadership/CMD.webp",
     title: "Address by Chairman & Managing Director",
     name: "Guiding the Future",
     body: "HPCL is not an organisation, HPCL is a family, HPCL is a way of life, HPCL is a spirit.",
   },
   {
-    img: "../images/leadership/DM.webp",
+    img: "https://82.112.226.178/images/leadership/DM.webp",
     title: "Address by Director Finance",
     name: "Inspiring Growth",
     body: "We are primarily a marketing company and as a marketing company customer comes first.",
   },
   {
-    img: "../images/leadership/DF.webp",
+    img: "https://82.112.226.178/images/leadership/DF.webp",
     title: "Address by Director Refinery",
     name: "Leading with Purpose",
     body: "Arise, awake & stop not, ill the goal is reached.",
   },
   {
-    img: "../images/leadership/DR.webp",
+    img: "https://82.112.226.178/images/leadership/DR.webp",
     title: "Address by Director Marketing",
     name: "Driving Success",
     body: "Leaders at our company prioritize teamwork and collaboration, ensuring that every voice is heard and valued.",
   },
   {
-    img: "../images/leadership/HR.webp",
+    img: "https://82.112.226.178/images/leadership/HR.webp",
     title: "Address by Director HR",
     name: "Building for the Future",
     body: "It is important we network with our channel partners and build on legacy of relaionship.",
   },
   {
-    img: "../images/leadership/CVO.webp",
+    img: "https://82.112.226.178/images/leadership/CVO.webp",
     title: "Address by Cheif Vigiliance Officer",
     name: "Leading by Example",
     body: "Our objective is to eliminate complaints all together.",
